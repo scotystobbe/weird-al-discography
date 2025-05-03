9f200bf6d9c2c840ea685bd0a3fdd1dba493d4b6
Scoty Stobbe <sstobbe@1800contacts.com>
2025-05-03 12:27:10 -0600

    move sort icon

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -134,7 +134,21 @@ export default function Home() {
             </button>
           )}
         </div>
-        {/* Spotify toggle switch always visible */}
+        {/* Sort icon only visible when searchTerm is empty, placed before the toggle */}
+        {!searchTerm && (
+          <button
+            type="button"
+            onClick={() => setShowFilters(f => !f)}
+            className="p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700 focus:outline-none"
+            aria-label="Show sorting options"
+          >
+            {/* Inline sort_icon.svg, theme-aware color */}
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-6 w-6 text-gray-500 dark:text-gray-300" fill="currentColor">
+              <path d="M9.25,5L12.5,1.75L15.75,5H9.25M15.75,19L12.5,22.25L9.25,19H15.75M8.89,14.3H6L5.28,17H2.91L6,7H9L12.13,17H9.67L8.89,14.3M6.33,12.68H8.56L7.93,10.56L7.67,9.59L7.42,8.63H7.39L7.17,9.6L6.93,10.58L6.33,12.68M13.05,17V15.74L17.8,8.97V8.91H13.5V7H20.73V8.34L16.09,15V15.08H20.8V17H13.05Z" />
+            </svg>
+          </button>
+        )}
+        {/* Spotify toggle switch always visible, now always last */}
         <div className="flex items-center">
           <button
             type="button"
@@ -159,20 +173,6 @@ export default function Home() {
             <span className="sr-only">Toggle Spotify search</span>
           </button>
         </div>
-        {/* Sort icon only visible when searchTerm is empty */}
-        {!searchTerm && (
-          <button
-            type="button"
-            onClick={() => setShowFilters(f => !f)}
-            className="p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700 focus:outline-none"
-            aria-label="Show sorting options"
-          >
-            {/* Inline sort_icon.svg, theme-aware color */}
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-6 w-6 text-gray-500 dark:text-gray-300" fill="currentColor">
-              <path d="M9.25,5L12.5,1.75L15.75,5H9.25M15.75,19L12.5,22.25L9.25,19H15.75M8.89,14.3H6L5.28,17H2.91L6,7H9L12.13,17H9.67L8.89,14.3M6.33,12.68H8.56L7.93,10.56L7.67,9.59L7.42,8.63H7.39L7.17,9.6L6.93,10.58L6.33,12.68M13.05,17V15.74L17.8,8.97V8.91H13.5V7H20.73V8.34L16.09,15V15.08H20.8V17H13.05Z" />
-            </svg>
-          </button>
-        )}
       </div>
       {showFilters && (
         <div className="flex gap-4 mb-4">
